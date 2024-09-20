eb47e316879abebcc14a9af1abe599365e63b4a9
YP10 <144463548+YP10@users.noreply.github.com>
2024-09-19 23:25:51 -0400

    done

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void helloWorld(){
+        String str="Hello World";
+        System.out.println(str);
+    }
+    public static void main(String [] args){
+        helloWorld();
+    }
+}
